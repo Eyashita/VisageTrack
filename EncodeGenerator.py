005919929f7722e0c37c3ae0a37b531325366671
eyashita_10 <eyashita.20bcd7027@vitap.ac.in>
2023-10-03 16:18:12 +0530

    Main project done

--- EncodeGenerator.py
+++ EncodeGenerator.py
@@ -2,6 +2,16 @@ import os
 import cv2
 import face_recognition
 import pickle
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import storage
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred,
+{
+    'databaseURL': "https://realtimefaceattendance-3c6e9-default-rtdb.firebaseio.com/",
+    'storageBucket': "realtimefaceattendance-3c6e9.appspot.com"
+})
 
 # Importing student images
 folderPath = 'Images'
@@ -11,9 +21,17 @@ imgList = []
 studentIds = []
 for path in pathList:
     imgList.append(cv2.imread(os.path.join(folderPath, path)))
+    studentIds.append(os.path.splitext(path)[0])
+
+    fileName = f'{folderPath}/{path}'
+    bucket = storage.bucket()
+    blob = bucket.blob(fileName)
+    blob.upload_from_filename(fileName)
+
+
+
     # print(path)
     # print(os.path.splitext(path)[0])
-    studentIds.append(os.path.splitext(path)[0])
 print(studentIds)
 
 def findEncodings(imagesList):
@@ -32,5 +50,5 @@ print("Encoding Complete")
 
 file = open("EncodeFile.p", 'wb')
 pickle.dump(encodeListKnownWithIds, file)
-file.close()mmmmmmmmmm
+file.close()
 print("File saved")
